23ecb0c1f0bc1c9bee6599ae5d1384601c33b380
PacioTenPros <paciobumbum@gmail.com>
2023-02-13 16:33:28 +0100

    Day 5 - Python loops

---
+++ day-5-loops/notes.py
@@ -0,0 +1,12 @@
+# fruits = ['Apple', 'Peach', 'Pear']
+
+# for fruit in fruits:
+#     print(f'{fruit} Pie')
+
+# sum = 0
+# for num in range(1, 101): # from 1 to 100
+#     sum += num
+# print(sum)
+
+for num in range(1, 11, 3): # from 1 to 100 where step = 3
+    print(num)
